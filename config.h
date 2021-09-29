5f19984f38a162ff7085fbb124b19fd21b9ce6b6
Colin M <colinm22283@isd273.org>
2021-09-29 15:40:33 -0400

    1.0

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <fstream>
+#include <map>
 
 class Config
 {
@@ -11,6 +12,8 @@ public:
     void* operator [] (std::string key);
 private:
     std::fstream* file;
+    std::map<std::string, int> intMap;
+    std::map<std::string, std::string> stringMap;
 };
 
 #ifndef JSON_FUNCTIONS_DEFINED
@@ -19,6 +22,8 @@ private:
 Config::Config(std::fstream* _file)
 {
     file = _file;
+
+    intMap["test"] = 4;
 }
 
 Config Config::loadFile(std::string path)
@@ -27,11 +32,13 @@ Config Config::loadFile(std::string path)
     _file.open(path);
 
     Config proto(&_file);
+
+    return proto;
 }
 
 void* Config::operator [] (std::string key)
 {
-
+    return (void*)(&intMap[key]);
 }
 
 #endif
